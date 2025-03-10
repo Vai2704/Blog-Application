544201e4123e0d8c70952c220f08c4c8a0ab567c
Vaibhav2704 <vaibhavsrivastava2025708@gmail.com>
2025-03-10 15:11:28 +0530

    add comments controller and views

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,5 +1,5 @@
 class PostController < ApplicationController
-  before_action :set_post, only: [:view, :edit, :update, :destroy]
+  # before_action :set_post
 
   def new
     @post = Post.new
@@ -17,34 +17,39 @@ class PostController < ApplicationController
   end
 
   def view
+    @post = Post.find(params[:id])
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+
     if @post.update(post_params)
       redirect_to post_path(@post), notice: 'Post updated successfully!'
     else
-      flash[:alert] = 'Error updating post.'
+      fladefsh[:alert] = 'Error updating post.'
       render :edit
     end
   end
 
   def destroy
+    @post = Post.find(params[:id])
     @post.destroy
     redirect_to root_path, notice: 'Post deleted successfully!'
   end
 
   private
 
-  def set_post
-    @post = Post.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
-    redirect_to root_path, alert: "Post not found."
-  end
+  # def set_post
+  #   @post = Post.find(params[:id])
+  # rescue ActiveRecord::RecordNotFound
+  #   redirect_to root_path, alert: "Post not found."
+  # end
 
-  def post_params
+  private def post_params
     params.require(:post).permit(:title, :content)
   end
 end
